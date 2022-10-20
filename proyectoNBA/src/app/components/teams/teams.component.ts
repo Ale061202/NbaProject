93b1b458fe5199abb355e37155ce67610e71bf1d
gcvictor22 <victorgonzaleztr22@triana.salesianos.edu>
2022-10-20 14:24:30 +0200

    html equipos empezado

--- proyectoNBA/src/app/components/teams/teams.component.ts
+++ proyectoNBA/src/app/components/teams/teams.component.ts
@@ -10,16 +10,27 @@ import { ListadoEquiposService } from 'src/app/services/listado-equipos.service'
 export class TeamsComponent implements OnInit {
 
   equiposLiga : Africa[] = []
+  equiposMarcador : Africa[] = []
 
   constructor(private teamService: ListadoEquiposService) { }
 
   ngOnInit(): void {
-  }
-
-  getTeams(){
     this.teamService.getPeople(2022).subscribe((resp) => {
       this.equiposLiga = resp.league.standard
+    
+      for (let i = 0; i < 9; i++) {
+        let rd = Math.trunc(Math.random() * (this.equiposLiga.length - 1) + 1)
+        if (i==0) {
+          this.equiposMarcador[i] = resp.league.standard[rd]
+        }else{
+          for (let j = 0; j < this.equiposMarcador.length; j++) {
+            if (resp.league.standard[rd].teamId == this.equiposMarcador[j].teamId) {
+            }else{
+              this.equiposMarcador[i] = resp.league.standard[rd]
+            }
+          }
+        }
+      }
     })
   }
-
 }
