cbd5af46c5502f9c72d16680350dcba43cce96f3
Alejandro <fernandez.goale21@triana.salesianos.edu>
2022-10-21 08:26:25 +0200

    adding app-routing

--- proyectoNBA/src/app/app-routing.module.ts
+++ proyectoNBA/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeamsComponent } from './components/teams/teams.component';
+import { PlayersListComponent } from './components/players-list/players-list.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: 'teams', component: TeamsComponent, pathMatch: 'full'},
+  {path: 'players', component: PlayersListComponent, pathMatch: 'full'}
+  {path: '', redirectTo: 'teams', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
